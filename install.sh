b0e4bbee00dae71f2ade7f7fc0c8b678883b9bdd
Endre Kadas <endre.kadas@cloudreach.com>
2019-01-04 16:28:02 +0000

    Added bash config and scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+
 BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
+OS=$(uname -s)
+
+# configures bash
+if [ "$OS" = "Darwin" ]; then
+   ln -sf $BASEDIR/bash/bash_mac ~/.bash_profile
+else
+   ln -sf $BASEDIR/bash/bash_linux ~/.bashrc
+fi
 
 # symlinks tmux configuration
 ln -sf $BASEDIR/tmux/tmux.conf ~/.tmux.conf
