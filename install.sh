c6d7fa78b8d088425a95e07e55feb450baa0ce78
ekadas <ekadas@gmail.com>
2019-01-04 21:42:19 +0000

    Added bash_profile for linux for tmux

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ if [ "$OS" = "Darwin" ]; then
    ln -sf $BASEDIR/bash/bash_mac ~/.bash_profile
 else
    ln -sf $BASEDIR/bash/bash_linux ~/.bashrc
+   ln -sf $BASEDIR/bash/bash_linux ~/.bash_profile
 fi
 
 # symlinks tmux configuration
