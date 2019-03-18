efe57b97592cc10e2de0488143a9e5c96604da7b
Endre Kadas <ekadas@gmail.com>
2019-03-18 15:35:37 +0000

    Added bash-completion for mac

--- bash/install.sh
+++ bash/install.sh
@@ -2,6 +2,7 @@
 
 if [ "$OS" = "Darwin" ]; then
    ln -sf $BASEDIR/bash/bash_mac ~/.bash_profile
+   brew install bash-completion
 else
    ln -sf $BASEDIR/bash/bash_linux ~/.bashrc
    ln -sf $BASEDIR/bash/bash_linux ~/.bash_profile
