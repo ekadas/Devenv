7f561d6ff7247f5f3ea32a4442a8795fdd15a0a7
Endre Kadas <ekadas@gmail.com>
2019-03-18 15:17:47 +0000

    Added installation of neovim

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+if [ "$OS" = "Darwin" ]; then
+   brew install neovim
+fi
+
+# install vim-plug
 curl --silent -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null
+# link config
+mkdir -p ~/.config/nvim
+ln -sf $BASEDIR/nvim/init.vim ~/.config/nvim/init.vim
+
 ## install python-neovim
 pyenv virtualenv -f $PYTHON2 nvim2 > /dev/null
 export PYENV_VERSION=nvim2
