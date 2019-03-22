78431cb737ede196f6be8b40063c8bce985f20a0
Endre Kadas <ekadas@gmail.com>
2019-03-22 12:04:16 +0000

    Added bash completion to docker

--- docker/install.sh
+++ docker/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 brew install docker-machine xhyve docker-machine-driver-xhyve
+curl -L https://raw.githubusercontent.com/docker/compose/1.23.2/contrib/completion/bash/docker-compose -o /usr/local/etc/bash_completion.d/docker-compose
+curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /usr/local/etc/bash_completion.d/docker.sh
 sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
 sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
