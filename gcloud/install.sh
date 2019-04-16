0ffb57bd424c7652966fb75977b779385f92f5e4
Endre Kadas <ekadas@gmail.com>
2019-04-16 11:26:58 +0100

    Username now not hardcoded for gcloud installation

--- gcloud/install.sh
+++ gcloud/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+USER=$(id -un)
+
 pyenv virtualenv -f $PYTHON2 gcloud > /dev/null
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 export CLOUDSDK_INSTALL_DIR=/usr/local/share
-export CLOUDSDK_PYTHON=/Users/Endre/.pyenv/shims/python
+export CLOUDSDK_PYTHON=/Users/$USER/.pyenv/shims/python
 export PYENV_VERSION=gcloud
 curl --silent https://sdk.cloud.google.com | bash > /dev/null
