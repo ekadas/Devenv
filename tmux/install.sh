d8c9674d65598619783a60392c600ecc89bf7051
Endre Kadas <ekadas@gmail.com>
2019-08-07 00:36:58 +0100

    Added tmux plugins

--- tmux/install.sh
+++ tmux/install.sh
@@ -5,4 +5,7 @@ if [ "$OS" = "Darwin" ]; then
 else
    sudo pacman -S tmux
 fi
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -sf $BASEDIR/tmux/tmux.conf ~/.tmux.conf
+~/.tmux/plugins/tpm/bin/install_plugins
