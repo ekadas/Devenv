b5a2dea3d18a3cbf1fc5500c95a998d9c64ac281
Endre Kadas <ekadas@gmail.com>
2019-08-07 19:31:24 +0100

    Added tmux plugin manager path

--- tmux/install.sh
+++ tmux/install.sh
@@ -8,4 +8,5 @@ fi
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -sf $BASEDIR/tmux/tmux.conf ~/.tmux.conf
+export TMUX_PLUGIN_MANAGER_PATH=~/.tmux/plugins/tpm/
 ~/.tmux/plugins/tpm/bin/install_plugins
