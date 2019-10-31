411d88fcf2da22e2c2efcb811b837ab26e387b4d
Endre Kadas <ekadas@gmail.com>
2019-10-31 17:54:56 +0000

    Updated iterm2 values

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PYTHON2=2.7.15
-PYTHON3=3.7.1
+PYTHON2=2.7.16
+PYTHON3=3.8.0
 
 BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 OS=$(uname -s)
@@ -26,7 +26,6 @@ confirm () {
    fi
 }
 
-# install brew
 if [ "$OS" = "Darwin" ]; then
    confirm "brew" brew/install.sh
 fi
@@ -35,44 +34,32 @@ if [ "$OS" = "Darwin" ]; then
    confirm "iterm2" iterm2/install.sh
 fi
 
-# configure python
 confirm "python" python/install.sh
 
-# configures bash
 confirm "bash" bash/install.sh
 source ~/.bash_profile
 
-# symlinks tmux configuration
 confirm "tmux" tmux/install.sh
 
-# configure git
 confirm "git" git/install.sh
 
-# configure nvim
-## install vim-plug
 confirm "neovim" nvim/install.sh
 
-# configure nvm
 confirm "nvm" nvm/install.sh
 
-# configure rbenv
 confirm "rbenv" rbenv/install.sh
 
-# configure rbenv
 confirm "rust" rust/install.sh
 
-# configure docker
 if [ "$OS" = "Darwin" ]; then
    confirm "docker" docker/install.sh
 fi
 
-# configure gcloud
 if [ "$OS" = "Darwin" ]; then
    # NOTE: this may require setting pyenv first
    confirm "gcloud" gcloud/install.sh
 fi
 
-# configure kubectl
 if [ "$OS" = "Darwin" ]; then
    confirm "kubectl" kubectl/install.sh
 fi
