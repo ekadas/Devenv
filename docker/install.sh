474311b591be1b5fdc5429e702884fb34a557006
Endre Kadas <ekadas@gmail.com>
2019-11-05 09:52:02 +0000

    Added core docker service installation

--- docker/install.sh
+++ docker/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 brew install docker-machine xhyve docker-machine-driver-xhyve
+brew cask install docker
 curl -L https://raw.githubusercontent.com/docker/compose/1.23.2/contrib/completion/bash/docker-compose -o /usr/local/etc/bash_completion.d/docker-compose
 curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /usr/local/etc/bash_completion.d/docker.sh
 sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
