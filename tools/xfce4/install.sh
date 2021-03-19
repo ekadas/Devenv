ea5075194f97dd7a43d3e6418c68c58169fc5e42
Endre Kadas <ekadas@gmail.com>
2021-03-19 21:48:44 +0000

    Add installation for xfce4 terminal config

---
+++ tools/xfce4/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$OS" = "Darwin" ]; then
+   echo "No installation instructions for mac"
+else
+   config_path=~/.config/xfce4/terminal
+   mkdir -p $config_path
+   rm $config_path/terminalrc
+   ln -sf $BASEDIR/tools/xfce4/terminalrc $config_path/terminalrc
+fi
