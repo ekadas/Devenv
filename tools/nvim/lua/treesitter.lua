3485edcaa4626a88b7602abe53392e0793a2b6d3
Endre Kadas <ekadas@gmail.com>
2021-06-17 23:12:11 +0100

    be explicit about which treesitter languages to install

--- tools/nvim/lua/treesitter.lua
+++ tools/nvim/lua/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-   ensure_installed = "all",
+   ensure_installed = { "bash", "comment", "css", "elm", "html", "java", "javascript", "json", "lua", "python", "rust", "toml", "typescript", "yaml" },
    ignore_install = { "haskell" },
    highlight = {
       enable = true,
