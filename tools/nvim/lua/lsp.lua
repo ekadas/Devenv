913db1676378d1cd8130bc1d050de2d095c512b4
Endre Kadas <ekadas@gmail.com>
2021-10-17 16:30:43 +0100

    Alias pip3 with pip

--- tools/nvim/lua/lsp.lua
+++ tools/nvim/lua/lsp.lua
@@ -9,7 +9,7 @@ sign_define('LspDiagnosticsSignError', {text='🩸'})
 
 local on_attach = function(client)
    if client.resolved_capabilities.document_formatting then
-      vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)]]
+      vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1500)]]
    end
 
    vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
