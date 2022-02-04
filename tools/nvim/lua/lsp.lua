21a929ecfe0841befa8201f433a02374b5c38e59
Endre Kadas <ekadas@gmail.com>
2022-02-04 20:45:25 +0000

    Updated goto_next lsp action

--- tools/nvim/lua/lsp.lua
+++ tools/nvim/lua/lsp.lua
@@ -14,7 +14,7 @@ local on_attach = function(client)
 
    vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
    vim.api.nvim_buf_set_keymap(0, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true})
-   vim.api.nvim_buf_set_keymap(0, 'n', '<space>', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', {noremap = true})
+   vim.api.nvim_buf_set_keymap(0, 'n', '<space>', '<cmd>lua vim.diagnostic.goto_next()<CR>', {noremap = true})
    vim.api.nvim_buf_set_keymap(0, 'n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', {noremap = true})
    vim.api.nvim_buf_set_keymap(0, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', {noremap = true})
    vim.api.nvim_buf_set_keymap(0, 'n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', {noremap = true})
