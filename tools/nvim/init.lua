3c6358e51ce2af4decc9117486dd581531c66667
Endre Kadas <ekadas@gmail.com>
2022-10-23 20:54:41 +0100

    refactor: Move plugins to init.lua

--- tools/nvim/init.lua
+++ tools/nvim/init.lua
@@ -109,8 +109,51 @@ opt.ttimeoutlen = 50
 -- enable spell checking
 autocmd({ 'BufRead', 'BufNewFile' }, { pattern = '*.md', command = 'setlocal spell spelllang=en_gb' })
 
--- set up lua plugins
-require('plugins')
+-- lsp
+require('lsp')
+
+-- treesitter
+require('treesitter')
+
+-- colorize
+require('colorizer').setup()
+
+-- commenter
+require('Comment').setup()
+
+-- completion
+require('completion')
+
+-- lines
+require('lualine').setup({
+   options = {
+      icons_enabled = false,
+      component_separators = '|',
+      section_separators = ''
+   },
+   sections = {
+      lualine_a = {'mode'},
+      lualine_b = {'FugitiveHead'},
+      lualine_c = {{'filename', path = 1}},
+      lualine_x = {'diagnostics'},
+      lualine_y = {'filetype'},
+      lualine_z = {'location'}
+   },
+   tabline = {
+      lualine_a = {{
+         'buffers',
+         show_filename_only = false,
+         buffers_color = {
+            active = { bg = 51, fg = 236 },
+            inactive = { bg = 236, fg = 51 }
+         },
+         symbols = {
+            modified = ' ●',
+            alternate_file = ''
+         }
+      }}
+   }
+})
 
 -- Nerdtree
 keymap('n', '<C-n>', ':NERDTreeToggle<CR>', { noremap = true })
