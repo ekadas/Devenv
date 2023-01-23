7d0e5b5719969130a815eb291db3cd75242f5c4d
Endre Kadas <ekadas@gmail.com>
2023-01-23 14:28:23 +0000

    chore: Update java 11

--- tools/java/install.sh
+++ tools/java/install.sh
@@ -4,7 +4,7 @@ if [ "$OS" = "Darwin" ]; then
    brew install openjdk@11
 
    brew install jenv
-   jenv add "$(brew --prefix)/Cellar/openjdk@11/11.0.17/libexec/openjdk.jdk/Contents/Home"
+   jenv add "$(brew --prefix)/Cellar/openjdk@11/11.0.18/libexec/openjdk.jdk/Contents/Home"
 
-   jenv global 11.0.17
+   jenv global 11.0.18
 fi
