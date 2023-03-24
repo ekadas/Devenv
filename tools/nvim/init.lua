f0d8b57967e7a7a2388208ba5c5302d7e6a78c71
Endre Kadas <ekadas@gmail.com>
2023-03-24 21:54:00 +0000

    feat: Add copilot to nvim

--- tools/nvim/init.lua
+++ tools/nvim/init.lua
@@ -36,6 +36,8 @@ Plug('hrsh7th/cmp-buffer')
 Plug('hrsh7th/cmp-path')
 Plug('hrsh7th/cmp-cmdline')
 Plug('hrsh7th/nvim-cmp')
+Plug('zbirenbaum/copilot.lua')
+Plug('zbirenbaum/copilot-cmp')
 
 -- treesitter
 Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
--- tools/nvim/lua/completion.lua
+++ tools/nvim/lua/completion.lua
@@ -1,5 +1,11 @@
 vim.o.completeopt = 'menuone,noinsert,noselect'
 
+require("copilot").setup({
+   suggestion = { enabled = false },
+   panel = { enabled = false },
+})
+require("copilot_cmp").setup()
+
 local has_words_before = function()
    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
@@ -18,8 +24,6 @@ cmp.setup({
       ['<Tab>'] = cmp.mapping(function(fallback)
          if cmp.visible() then
             cmp.select_next_item()
-         elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
          elseif has_words_before() then
             cmp.complete()
          else
@@ -29,8 +33,6 @@ cmp.setup({
       ['<S-Tab>'] = cmp.mapping(function(fallback)
          if cmp.visible() then
             cmp.select_prev_item()
-         elseif luasnip.jumpable( -1) then
-            luasnip.jump( -1)
          else
             fallback()
          end
@@ -42,6 +44,7 @@ cmp.setup({
       end
    },
    sources = {
+      { name = "copilot", keyword_length = 0 },
       { name = 'nvim_lsp' },
       { name = 'luasnip' },
       { name = 'buffer' }
